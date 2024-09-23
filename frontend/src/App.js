9680071573f75833bcfd4931305035ffcf546bc2
noam_wsl <winnersalmostwin@gmail.com>
2024-09-23 18:58:37 -0300

    added environments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,22 @@ import BackendHealthIndicator from './components/BackendHealthIndicator';
 import BackendStatus from './components/BackendStatus';
 
 function App() {
+  let environment;
+  if (process.env.NODE_ENV === 'production') {
+    environment = 'production';
+  } else if (process.env.NODE_ENV === 'development') {
+    environment = 'development';
+  } else {
+    environment = 'staging';
+  }
+
+  const baseName = `/personal_website/${environment}`;
+
+  console.log('Current environment:', environment);
+  console.log('Current basename:', baseName);
+
   return (
-    <Router>
+    <Router basename={baseName}>
       <div className="App">
         <Header />
         <BackendHealthIndicator />
