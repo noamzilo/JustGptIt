e5aac5c55f2bad53ed034142714090681fe14f9e
noam_wsl <winnersalmostwin@gmail.com>
2024-10-09 18:34:06 -0300

    validating creds file existence

--- backend/docker_entrypoint.sh
+++ backend/docker_entrypoint.sh
@@ -4,13 +4,16 @@
 if [ -n "$GCP_SA_KEY" ]; then
     echo "Decoding credentials from environment variable..."
     echo "$GCP_SA_KEY" | base64 -d > /app/gcp-credentials.json
-#else
-#    echo "Decoding credentials from local file..."
-#    base64 -d /app/gcp-sa-key.json.base64 > /app/gcp-credentials.json
-#fi
 else
     echo "NO GCP_SA_KEY environment variable"
-    exit(1)
+    if [ -f "/app/gcp-sa-key.json.base64" ]; then
+        echo "Decoding credentials from local file..."
+        base64 -d /app/gcp-sa-key.json.base64 > /app/gcp-credentials.json
+    else
+        echo "ERROR: No credentials found"
+        exit 1
+    fi
+fi
 
 # Run the original command (this would typically be your application start command)
 exec "$@"
