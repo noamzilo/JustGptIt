6be9f6f29e3062d3a1bd67ea33b24c5b1c1727f4
noam_wsl <winnersalmostwin@gmail.com>
2024-10-09 14:34:49 -0300

    fixed the local docker to argentina time and created version endpoint. For some reason hello still exists, likely cache

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,7 +7,7 @@ print("URLs module loaded", file=sys.stderr)
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
-    path('version', views.hello_world, name='version'),
+    path('version', views.version, name='version'),
     path('projects/', views.ProjectList.as_view(), name='project-list'),
     path('health', views.health_check, name='health_check'),
     path('test-gcp', views.test_gcp, name='test_gcp'),
