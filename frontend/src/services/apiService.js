944ce3ef587c8808d5baec7807447d0a50a445fc
noam_wsl <winnersalmostwin@gmail.com>
2024-10-19 18:20:32 -0300

    syntax

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 
 const apiService = {
-  get: async (endpoint) => {``
+  get: async (endpoint) => {
     try {
       const response = await axios.get(`${API_URL}${endpoint}`);
       return response.data;
