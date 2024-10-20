8028fcca96be89f8a2314a08c4eaec44a4a08851
noam_wsl <winnersalmostwin@gmail.com>
2024-10-20 20:22:06 -0300

    LOCAL FRONT CONNECTS TO LOCAL BACK

--- backend/docker_entrypoint.sh
+++ backend/docker_entrypoint.sh
@@ -16,7 +16,7 @@ echo "Collecting static files..."
 python manage.py collectstatic --noinput -v 2
 
 # Start the application with verbose logging
-echo "Starting the application..."
-echo "env vars:"
+echo "Starting the application... (docker_entrypoint.sh is done)"
+# echo "env vars:"
 # printenv
 exec gunicorn --bind 0.0.0.0:8080 --log-level debug --capture-output --enable-stdio-inheritance --error-logfile /var/log/gunicorn-error.log --access-logfile /var/log/gunicorn-access.log config.wsgi:application
