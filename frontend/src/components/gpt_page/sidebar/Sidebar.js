0e61927d448fccfc27533956ed38b321104fc55f
noam_wsl <winnersalmostwin@gmail.com>
2024-10-30 11:54:06 -0300

    side bar looks fine

--- frontend/src/components/gpt_page/sidebar/Sidebar.js
+++ frontend/src/components/gpt_page/sidebar/Sidebar.js
@@ -9,14 +9,16 @@ function Sidebar() {
                 <button className={styles.closeSidebarButton}>☰</button>
                 <button className={styles.newChatButton}>＋</button>
             </div>
-            <div className={styles.sidebarPinnedItems}>
-                <div className={styles.logo}>Pinned1</div>
-                <div className={styles.logo}>Pinend2</div>
-            </div>
+            <div className={styles.sidebarContent}>
+                <div className={styles.sidebarPinnedItems}>
+                    <div className={styles.logo}>Pinned1</div>
+                    <div className={styles.logo}>Pinend2</div>
+                </div>
 
-            <div className={styles.sidebarRecentChats}>
-                <div className={styles.sidebarRecentChat}>Chat1</div>
-                <div className={styles.sidebarRecentChat}>Chat2</div>
+                <div className={styles.sidebarRecentChats}>
+                    <div className={styles.sidebarRecentChat}>Chat1</div>
+                    <div className={styles.sidebarRecentChat}>Chat2</div>
+                </div>
             </div>
             <div className={styles.footer}>Upgrade plan</div>
         </aside>
