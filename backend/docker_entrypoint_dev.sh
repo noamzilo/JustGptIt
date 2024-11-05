f3a7d57ce734f0ebec61b163d9d70d69d734041e
noam_wsl <winnersalmostwin@gmail.com>
2024-11-05 00:22:45 -0300

    attempt to make django changes not need server restart

--- backend/docker_entrypoint_dev.sh
+++ backend/docker_entrypoint_dev.sh
@@ -5,6 +5,29 @@ source /app/docker_entrypoint_common.sh
 
 # Development-specific functionality
 
+# Start Gunicorn with reload and debugpy for debugging
+echo "Starting Gunicorn with reload and debugpy..."
+
+# ENV DJANGO_SETTINGS_MODULE=config.settings
+# ENV DJANGO_AUTORELOAD_MODE=stat
+
+# Start debugpy in the background
+# python -m debugpy --listen 0.0.0.0:5678 --wait-for-client &
+
+# # Start Gunicorn with reload
+# exec gunicorn --reload \
+#     --bind 0.0.0.0:8080 \
+#     --log-level debug \
+#     --capture-output --enable-stdio-inheritance \
+#     --error-logfile /var/log/gunicorn-error.log \
+#     --access-logfile /var/log/gunicorn-access.log config.wsgi:application
+#!/bin/bash
+
+# Source the common entrypoint script
+source /app/docker_entrypoint_common.sh
+
+# Development-specific functionality
+
 # Start Django development server with debugpy for debugging
 echo "Starting Django development server with debugpy..."
-exec python -m debugpy --listen 0.0.0.0:5678 manage.py runserver 0.0.0.0:8080
+exec python -m debugpy --listen 0.0.0.0:5678 manage.py runserver 0.0.0.0:8080 --reload
