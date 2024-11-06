149197e0309d1ca4f1f1374c320f1a60d5ccdb68
noam_wsl <winnersalmostwin@gmail.com>
2024-11-06 16:30:21 -0300

    just have to pass the response to the interactive chat now

--- frontend/src/components/gpt_page/main_content/MainContent.js
+++ frontend/src/components/gpt_page/main_content/MainContent.js
@@ -25,10 +25,10 @@ const MainContent = () => {
                     ? <h2>{GPT_PAGE_CONSTANTS.QUERY_SECTION_TEXT}</h2>
                     : null}
                 {
-                    // isInitialChat
-                    // ? <InitialChat onTypingAnimationDone={handleTypingAnimationDone} />
-                    // :
-                    < ResponseChat query="Hello is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange" response="Hi there! is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange" />
+                    isInitialChat
+                        ? <InitialChat onTypingAnimationDone={handleTypingAnimationDone} />
+                        :
+                        < ResponseChat query="Hello is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange" response="Hi there! is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange" />
                 }
             </section>
 
--- frontend/src/components/gpt_page/main_content/chat/ChatMessage.jsx
+++ frontend/src/components/gpt_page/main_content/chat/ChatMessage.jsx
@@ -1,17 +1,49 @@
-// Component which represents each chat message in the chat window
-// Each message takes up all of the width of the parent container
-
-import React from 'react';
+import React, { useEffect } from 'react';
+import { motion } from 'framer-motion';
+import mitt from 'mitt';
 import styles from './ChatMessage.module.css';
 
+const emitter = mitt();
+
+const typingAnimation = {
+    hidden: { display: 'none' },
+    visible: { display: 'inline' },
+};
+
 function ChatMessage({ message, isUser }) {
+    useEffect(() => {
+        emitter.emit('typingCompleted');
+    }, [message]);
+
     return (
         <div className={`${styles.messageListItem} ${isUser ? styles.userMessage : styles.assistantMessage}`}>
             <div className={styles.messageContent}>
-            <div className={styles.messageText}>{message}</div>
+                {isUser ? (
+                    <div className={styles.messageText}>{message}</div>
+                ) : (
+                    <motion.div
+                        className={styles.messageText}
+                        initial="hidden"
+                        animate="visible"
+                        variants={{
+                            visible: {
+                                transition: {
+                                    staggerChildren: 0.01, // Controls typing speed
+                                },
+                            },
+                        }}
+                    >
+                        {message.split('').map((char, index) => (
+                            <motion.span key={index} variants={typingAnimation}>
+                                {char}
+                            </motion.span>
+                        ))}
+                    </motion.div>
+                )}
             </div>
         </div>
     );
 }
 
 export default ChatMessage;
+export { emitter };
