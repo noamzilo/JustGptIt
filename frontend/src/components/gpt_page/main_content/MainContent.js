c677fe2ab8bf68031d022ca5b043a06b1fffd268
noam_wsl <winnersalmostwin@gmail.com>
2024-11-07 13:36:24 -0300

    showing response chat immediately with "waiting" signal which isn't pretty

--- frontend/src/components/gpt_page/main_content/MainContent.js
+++ frontend/src/components/gpt_page/main_content/MainContent.js
@@ -42,7 +42,7 @@ const MainContent = () => {
                     ? <h2>{GPT_PAGE_CONSTANTS.QUERY_SECTION_TEXT}</h2>
                     : null}
                 {
-                    !initialChatDone ?
+                    !isInitialChatDoneAnimating ?
                         <InitialChat onTypingAnimationDone={handleTypingAnimationDone} onLlmResponse={onLlmResponse} />
                         :
                         // < ResponseChat query="Hello is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange" response="Hi there! is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchange" />
--- frontend/src/components/gpt_page/main_content/chat/ResponseChat.jsx
+++ frontend/src/components/gpt_page/main_content/chat/ResponseChat.jsx
@@ -3,12 +3,17 @@ import styles from "./ResponseChat.module.css";
 import ChatMessage from "./ChatMessage";
 
 function ResponseChat({ query, response }) {
-  return (
+    //response should display "..." while waiting for response
+    if (response === "") {
+        response = "...";
+    }
+
+    return (
     <div className={styles.chatContainer}>
         <ChatMessage message={query} isUser={true} />
         <ChatMessage message={response} isUser={false} />
     </div>
-  );
+    );
 }
 
 export default ResponseChat;
