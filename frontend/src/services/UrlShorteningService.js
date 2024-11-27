124bf62bb7e286dc00fd582a3a9f94ce01544b34
noam_wsl <winnersalmostwin@gmail.com>
2024-11-26 21:12:50 -0300

    url hash was snot the correct api

--- frontend/src/services/UrlShorteningService.js
+++ frontend/src/services/UrlShorteningService.js
@@ -15,7 +15,7 @@ const UrlShorteningService = {
 			if (response.status !== 200) {
 				throw new Error(`URL Shortening failed: ${response.data}`);
 			}
-			const url_hash = response.data.url_hash;
+			const url_hash = response.data.hash;
 			const short_url = window.location.hostname + '/r/' + url_hash;
 			console.log(`UrlShorteningService: received short URL ${short_url}`);
 			return short_url;
